ea2f32590996e71afa8de2f42007bb08c7f19e42
Ayushi <167096421+sushinsoda@users.noreply.github.com>
2026-01-03 18:00:00 +0000

    Day 1: printing and strings

---
+++ learning/day01_printing_and_strings.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name generator")
+name1 = input("What is the city you grew up in?\n")
+name2 = input("What is the name of your first pet?\n")
+print("Your band name is " + name1 + " " + name2)
